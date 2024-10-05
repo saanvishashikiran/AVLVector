78824e0b3cd693883bb62406b894b70f68cd09c2
saanvishashikiran <ss4878@georgetown.edu>
2024-10-04 22:46:41 -0400

    Starting, wrote tentative class declaration and file structure

---
+++ AVL.h
@@ -0,0 +1,73 @@
+#ifndef AVL_H
+#define AVL_H
+
+#include <vector>
+#include <utility>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+
+/****************************************************************************
+ *                                                                          *
+ *                       class AVLTree declaration                          *
+ *                                                                          *
+ ****************************************************************************/
+
+class AVLTree {
+    private:
+    struct Node {
+        int value;
+        int height;
+        Node* left;
+        Node* right;
+        Node* parent;
+
+        //node constructor
+        Node(int val) : value(val), height(1), left(nullptr), right(nullptr), parent(nullptr) {}
+    };
+
+    typedef Node* nodeptr;
+    nodeptr root = nullptr;
+
+    //need to figure out which private member functions for AVL properities are needed
+
+    public:
+    AVLTree() : root(nullptr) {} //default constructor
+    ~AVLTree();
+
+    void insert(int value);
+    void remove(int value);
+    //idk figure out which other functions are needed for this
+};
+
+
+
+
+/****************************************************************************
+ *                                                                          *
+ *                       class AVLVector declaration                        *
+ *                                                                          *
+ ****************************************************************************/
+
+class AVLVector {
+    private:
+    AVLTree tree;
+
+    public:
+    AVLVector();
+    ~AVLVector();
+
+    int elementAtRank(int r);
+    bool replaceAtRank(int r, int e);
+    bool insertAtRank(int r, int e);
+    bool removeAtRank(int r);
+    int rankOf(int e);
+    void printAll();
+};
+
+
+
+#endif
